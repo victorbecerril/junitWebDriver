a0313d7361a5ee378f888939bd75408e2ab44740
victorbecerril <victor.becerril.t@gmail.com>
2018-05-03 10:26:05 -0500

    Amazon excercise completed

--- src/amazonProducts/GetAmazonProductsBaseTest.java
+++ src/amazonProducts/GetAmazonProductsBaseTest.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -30,19 +31,51 @@ public class GetAmazonProductsBaseTest {
 	}
 	
 	protected void getProducts() {
-		List<WebElement> productList = driver.findElements(By.xpath("//li[contains(@id, 'result')]"));
+		List<WebElement> productList = driver.findElements(By.xpath("//li[starts-with(@id, 'result')]"));
 		
 		for (WebElement productElem : productList) {
-			WebElement productName = productElem.findElement(By.xpath(".//h2[contains(@class, 'a-size-medium')]"));
-			System.out.println(productName.getText());
-			WebElement productPrice = productElem.findElement(By.xpath(".//span[contains(@class, 'sx-price')"));
-			System.out.println(productPrice.getText().toString());
+			System.out.println(productElem.getAttribute("Id"));
+			try {
+				WebElement productName = productElem.findElement(By.xpath(".//h2[contains(@class, 'a-size-medium')]"));
+				System.out.println("Item: " + productName.getText());
+			} catch (Exception e1) {
+				System.out.println("No se encontro el nombre del articulo");
+			}
+			WebElement productPrice = productElem.findElement(By.xpath(".//span[contains(@class, 'sx-price')]"));
+			String formattedPrice = formatPrice(productPrice.getText());
+			System.out.println("Price: " + formattedPrice);
+			WebElement productRating;
+			try {
+				productRating = productElem.findElement(By.xpath(".//*[contains(text(), 'out of 5 stars')]"));
+				String ratingText = productRating.getAttribute("textContent");
+				String rating = getRating(ratingText);
+				System.out.println("Rating: " + rating);
+			} catch (NoSuchElementException e) {
+				// TODO Auto-generated catch block
+				System.out.println("No Rating Provided");
+			}
 			System.out.println();
 		}
 		
 	}
 
 	
+	private String getRating(String ratingText) {
+		// TODO Auto-generated method stub
+		//X.X out of 5 stars
+		int firstSpaceIndex = ratingText.indexOf(" ");
+		String rating = ratingText.substring(0, firstSpaceIndex);
+		return rating;
+	}
+
+	private String formatPrice(String text) {
+		// TODO Auto-generated method stub
+		//"$ XXX XX"
+		String[] priceElements = text.split(" ");
+		String formattedPrice = priceElements[0] + priceElements[1] + "." + priceElements[2];
+		return formattedPrice;
+	}
+
 	@After
 	public void tearDown() {
 		driver.quit();
